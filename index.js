0d11be7cbd6728a0848c962a1517588cc4d3044b
Jennifer Joseph <josephjenny490@gmail.com>
2022-09-14 12:33:22 -0600

    building RESTFUL API's for mongoDB

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const courseRoutes = require('./routes/index')
+// const express = require('express');
+// const helmet = require('helmet');
+// const morgan = require('morgan');
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+// const courseRoutes = require('./routes/index')
 
-const app = express();
-//mongodb configuration
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+// const app = express();
+// //mongodb configuration
+// mongoose.connect(process.env.MONGO_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
 
-  }).then(()=>console.log('connected to mongoDb. . .'))
-  .catch((err)=>console.log(err.message))
+//   }).then(()=>console.log('connected to mongoDb. . .'))
+//   .catch((err)=>console.log(err.message))
 
 
 
-if(app.get('env')==='development'){
-    app.use(morgan('tiny'));
-};
-app.use(express.json());
-app.use(helmet());
-app.use('/', courseRoutes)
+// if(app.get('env')==='development'){
+//     app.use(morgan('tiny'));
+// };
+// app.use(express.json());
+// app.use(helmet());
+// app.use('/', courseRoutes)
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listening on port ${port}...`))
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => console.log(`listening on port ${port}...`))
 
 
--- routes/courses.js
+++ routes/courses.js
@@ -1,109 +1,100 @@
-const express = require('express');
-const courseModel = require('../models/courses')
-const data = require('../exercise-data.json')
-const router = express.Router()
-const joi = require('joi');
+// const express = require('express');
+// const courseModel = require('../models/courses')
+// const data = require('../exercise-data.json')
+// const router = express.Router()
+// const joi = require('joi');
 
 
-router.post('/createCourse', async (req, res)=>{
- for (let i = 0; i < data.length;  i++) {
-        const dataSchema = {
-            tags: data[i].tags,
-            date: data[i].date,
-            name: data[i].name,
-            author: data[i].author,
-            isPublished: data[i].isPublished,
-            price: data[i].price,
-        }
-        const newCourse = new courseModel(dataSchema)
-        await newCourse.save()
-    }
+// router.post('/createCourse', async (req, res)=>{
+//  for (let i = 0; i < data.length;  i++) {
+//         const dataSchema = {
+//             tags: data[i].tags,
+//             date: data[i].date,
+//             name: data[i].name,
+//             author: data[i].author,
+//             isPublished: data[i].isPublished,
+//             price: data[i].price,
+//         }
+//         const newCourse = new courseModel(dataSchema)
+//         await newCourse.save()
+//     }
  
-    res.status(201).json({
-        status:true,
-        message: 'the courses have been created'
-    })
-})
+//     res.status(201).json({
+//         status:true,
+//         message: 'the courses have been created'
+//     })
+// })
 
-router.get('/getCourses', async(req, res)=>{
-    const allCourses = await courseModel.find()
-        res.status(200).json({
-            status:true,
-            message: 'the courses have been fetched succesfully',
-            data: allCourses,
-        })
+// router.get('/getCourses', async(req, res)=>{
+//     const allCourses = await courseModel.find()
+//         res.status(200).json({
+//             status:true,
+//             message: 'the courses have been fetched succesfully',
+//             data: allCourses,
+//         })
 
-})
-//get one course
+// })
+// //get one course
 
-router.get('/getOneCourse/:id', async (req, res)=>{
+// router.get('/getOneCourse/:id', async (req, res)=>{
 
-    try {
-    const allCourses = await courseModel.findById(req.params.id) 
+//     try {
+//     const allCourses = await courseModel.findById(req.params.id) 
 
-    res.status(200).json({
-        status:true,
-        message:'the course with the given ID . . .',
-        data:allCourses,
-    })
-    } catch (error) {
-        return res.status(404).json({
-            status: false,
-            message:'the course with the given ID was not found',
-          error:  error.message
-        })
+//     res.status(200).json({
+//         status:true,
+//         message:'the course with the given ID . . .',
+//         data:allCourses,
+//     })
+//     } catch (error) {
+//         return res.status(404).json({
+//             status: false,
+//             message:'the course with the given ID was not found',
+//           error:  error.message
+//         })
        
-    }
+//     }
 
-})
+// })
 
-//UPDATING A COURSE
-router.patch('/update/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const updatedData = req.body;
-        const options = { new: true };
+// //UPDATING A COURSE
+// router.patch('/update/:id', async (req, res) => {
+//     try {
+//         const id = req.params.id;
+//         const updatedData = req.body;
+//         const options = { new: true };
 
-        const allCourses = await courseModel.findByIdAndUpdate(
-            id, updatedData, options
-        )
+//         const allCourses = await courseModel.findByIdAndUpdate(
+//             id, updatedData, options
+//         )
 
-        res.send(allCourses)
-    }
-    catch (error) {
-        res.status(400).json({ 
-            status:false,
-            message: error.message })
-    }
-})
+//         res.send(allCourses)
+//     }
+//     catch (error) {
+//         res.status(400).json({ 
+//             status:false,
+//             message: error.message })
+//     }
+// })
 
-//DELETE A COURSE
-router.delete('/delete/:id', async (req, res) => {
-    try {
-        const allCourses = await courseModel.findByIdAndDelete(req.params.id)
-        res.send(`the course with ${data.name} has been deleted..`)
-    }
-    catch (error) {
-        res.status(400).json({ 
-            status:false,
-            message: error.message })
-    }
-})
-
-
-//UPDATING A COURSE
+// //DELETE A COURSE
+// router.delete('/delete/:id', async (req, res) => {
+//     try {
+//         const allCourses = await courseModel.findByIdAndDelete(req.params.id)
+//         res.send(`the course with ${data.name} has been deleted..`)
+//     }
+//     catch (error) {
+//         res.status(400).json({ 
+//             status:false,
+//             message: error.message })
+//     }
+// })
 
 
+// //UPDATING A COURSE
 
 
- 
-
-
-
-
-
-
-module.exports = router 
+ module.exports = router 
 
 
 
